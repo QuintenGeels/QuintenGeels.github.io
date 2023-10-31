6302e1dfc028cec380ed0307d9aa88cc52ccfaa4
QGeels <94874270+QGeels@users.noreply.github.com>
2023-10-31 15:50:09 +0100

    added the last files

--- js/myscript.js
+++ js/myscript.js
@@ -3,6 +3,8 @@ console.log("hello world");
 document.getElementById("title-item").onclick = function() {changeColor()};
 
 
+
+
 function changeColor() {
     document.getElementById("title-item").style.color = "white";
 }
